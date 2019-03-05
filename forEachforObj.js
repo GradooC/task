51c3f7e6541fc71ff8cf48706059412e4120d98a
Evgeniy Shchemeliov <e.shchemeliov@andersenlab.com>
2019-03-05 16:48:28 +0300

    Переписал реализацию в ES5 синтаксисе

--- forEachforObj.js
+++ forEachforObj.js
@@ -1,9 +1,12 @@
 // Написать реализацию метода forEach для объектов
 Object.prototype.forEach = function (fn) {
-  return Object.keys(this).reduce((acc, key) => ({...acc, [key]: fn(this[key])}), {});
+  return Object.keys(this).reduce((acc, key) => {
+    acc[key] = fn(this[key]);
+    return acc;
+  }, {})
 };
 
 //Вычислить Math.max на массиве
 Array.prototype.max = function() {
-  return Math.max(...this);
+  return Math.max.apply(null, this);
 };
